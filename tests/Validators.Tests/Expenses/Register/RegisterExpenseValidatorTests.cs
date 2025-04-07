bbf9a221d52f37a52f6b3de87adaeb011005b4b4
ronaldo porto <cabralporto@outlook.com>
2025-04-07 17:55:38 -0300

    feat(tests): add validator tests for expense registration
    - Create CommonTestUtilities project with test helpers
    - Add RequestRegisterExpenseJsonBuilder for generating test data
    - Implement RegisterExpenseValidatorTests to validate expense registration
    - Set up test project structure with necessary dependencies

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Xunit;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        
+       result.IsValid.Should().BeTrue();
+
+    }
+}
